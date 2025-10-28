abf5aa7fc1c4608121d4a83953a869d31022bbb8
HanXD345 <manpreet345@yahoo.com>
2025-10-28 13:00:01 -0400

    Add comments to code

--- src/server.js
+++ src/server.js
@@ -9,17 +9,21 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(express.json());
+
+app.use(express.json()); // Middleware --> handles JSON parsing
+
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // allow frontend
+      credentials: true, // if cookies/auth headers are needed
+    })
+  );
+}
 
 app.use("/users", usersRoute);
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", // allow frontend
-    credentials: true, // if cookies/auth headers are needed
-  })
-);
-
+// Start serving only after connecting to DB
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`App started at: http://localhost:${PORT}`);
